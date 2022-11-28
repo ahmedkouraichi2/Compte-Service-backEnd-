94da5142d78c3c64ad2e14fa464d35082e6e3ed6
Your Name <kouraichiahmed2@gmail.com>
2022-11-28 15:07:56 +0100

    repositoryRessource

--- src/main/java/com/enotes/repository/CompteRepository.java
+++ src/main/java/com/enotes/repository/CompteRepository.java
@@ -1,10 +1,15 @@
 package com.enotes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.enotes.entity.Compte;
 
-// specifier le type  compte de Id Lonte
+
+
+@RepositoryRestResource
 public interface CompteRepository extends JpaRepository<Compte,Long> {
 
+	
+	
 }
